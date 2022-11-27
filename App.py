26357166da57b615e6b837b3ea127eef948b7e1d
Elaija Heart Velasco <21-01226@g.batstate-u.edu.ph>
2022-11-27 17:54:04 +0800

    Create App.py

---
+++ App.py
@@ -0,0 +1,17 @@
+import csv
+student_fields = ['roll', 'srcode', 'fname', 'age','program', 'year']
+student_database = 'student.csv'
+
+def show_menu():
+    print ('Student Management System')
+    print ('Main Menu:')
+    print ('1 - Add Student')
+    print ('2 - View Student List')
+    print ('3 - Delete Student Information')
+    print ('4 - Search Student Information')
+    print ('5 - Update Student Information')
+    print ('6 - Exit App')
+
+def add_student():
+    pass
+
