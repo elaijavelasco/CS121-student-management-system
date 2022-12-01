1d015f77c38fb60bd00096c62bfcc4e67f4964cb
Elaija Heart Velasco <21-01226@g.batstate-u.edu.ph>
2022-12-01 21:27:19 +0800

    Add files via upload

--- App.py
+++ App.py
@@ -54,15 +54,20 @@ def showMenu():
 
 
 def addStudent():
-    print("\n\n\t\t\t** Add Student Information **\n")
+    print("\n\n\t\t\t** Add Student Record **\n")
     global student_fields
     global student_database
 
 
     student_data = []
+
+    print("\n",71*'=')
+    print("\n\t\t\t >> Fill-in Information << \n")
+
     for field in student_fields:
         value = input("\t\t\tEnter " + field + ": ")
         student_data.append(value)
+    print("\n",71*'=')
 
     with open(student_database, "a", encoding="utf-8") as f:
         writer = csv.writer(f)
@@ -100,21 +105,21 @@ def searchStudent():
     global student_fields
     global student_database
 
-    print("\n\t\t\t** Search Student Information **\n")
+    print("\n\n\t\t\t** Search Student Record **\n")
     roll = input("\t\t\tEnter roll number to search: ")
     with open(student_database, "r", encoding="utf-8") as f:
         reader = csv.reader(f)
         for row in reader:
             if len(row) > 0:
                 if roll == row[0]:
-                    print("\n\t\t\t",34*'=')
-                    print("\n\t\t\t\t   Student Record\n")
-                    print("\t\t\t    Roll: ", row[0])
-                    print("\t\t\t    Full Name: ", row[1])
-                    print("\t\t\t    ID Number: ", row[2])
-                    print("\t\t\t    Program: ", row[3])
-                    print("\t\t\t    Year Level: ", row[4])
-                    print("\n\t\t\t",34*'=')
+                    print("\n",71*'=')
+                    print("\n\t\t\t   >> Student Record << \n")
+                    print("\t\t\t Roll: ", row[0])
+                    print("\t\t\t Full Name: ", row[1])
+                    print("\t\t\t ID Number: ", row[2])
+                    print("\t\t\t Program: ", row[3])
+                    print("\t\t\t Year Level: ", row[4])
+                    print("\n",71*'=')
                     break
         else:
             print("\n\n\t\t\tRoll number not found in our database!")
@@ -126,7 +131,7 @@ def updateStudent():
     global student_fields
     global student_database
 
-    print("\n\t\t\t** Update Student Information **\n")
+    print("\n\n\t\t\t** Update Student Information **\n")
     roll = input("\t\t\tEnter roll no. to update: ")
     index_student = None
     updated_data = []
@@ -137,18 +142,21 @@ def updateStudent():
             if len(row) > 0:
                 if roll == row[0]:
                     index_student = counter
-                    print("Student found at index [",index_student, "] ")
+                    print("\n\t\t\tStudent found at index [",index_student, "] ")
+                    print("\n",71*'=')
+                    print("\n\t\t\t>> Fill-in New Record << \n")
                     student_data = []
                     for field in student_fields:
-                        value = input("Enter " + field + ": ")
+                        value = input("\t\t\tEnter " + field + ": ")
                         student_data.append(value)
                     updated_data.append(student_data)
+                    print("\n",71*'=')
+                    
+                    print
                 else:
                     updated_data.append(row)
                 counter += 1
 
-
-    
     if index_student is not None:
         with open(student_database, "w", encoding="utf-8") as f:
             writer = csv.writer(f)
@@ -156,14 +164,16 @@ def updateStudent():
     else:
         print("\n\t\t\tRoll No. not found in our database.\n")
 
+    print("\n\n\t\t\t** Data saved successfully! **")
     input("\n\t\t\tPress any key to continue...")
+    print("\n")
 
 
 def deleteStudent():
     global student_fields
     global student_database
 
-    print("\n\t\t\t ***Delete Student Information***\n")
+    print("\n\n\t\t\t ***Delete Student Information***\n")
     roll = input("\t\t\tEnter roll number to delete: ")
     student_found = False
     updated_data = []
@@ -182,7 +192,10 @@ def deleteStudent():
         with open(student_database, "w", encoding="utf-8") as f:
             writer = csv.writer(f)
             writer.writerows(updated_data)
-        print("Roll no. ", roll, "deleted successfully!")
+        print("\n",71*'=')
+        print("\n\t\t\tRoll no. ", roll, "deleted successfully!")
+        print("\n",71*'=')
+        
     else:
         print("\n\t\t\tRoll number not found in our database! \n")
 
